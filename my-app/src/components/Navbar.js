bb3aea17ef097350a52cdea3c5725ac6b635785d
UbuntuJax <jacksonbledsoe@gmail.com>
2023-09-02 23:34:47 +1000

    end of 02.09

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -2,13 +2,44 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
+    const [click, setClick] = useState(false);
+  
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
             <Link to="/" className="navbar-logo">
-                TRVL <i className="fab fa-typo3"></i>
+                "New" Jackson Just Dropped <i className="fab fa-typo3"></i>
             </Link>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Why?
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                        Gimme Gimme Gimme T-shirt
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                        Rick Roll
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                        Lets Play Tic Tac Toe
+                    </Link>
+                </li>
+            </ul>
+
+
         </div>
     </nav>
     </>
